b594acfb677d1ce2ffe2a331079ba79dc98357c7
industriously <dev.rojiwon@gmail.com>
2023-02-21 05:10:03 +0900

    db schema update

--- prisma/migrations/20230221161836_/migration.sql
+++ prisma/migrations/20230221161836_/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
     `sub` VARCHAR(191) NOT NULL,
     `oauth_type` VARCHAR(191) NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `User` (
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
 
-    UNIQUE INDEX `User_email_key`(`email`),
-    UNIQUE INDEX `User_sub_oauth_type_key`(`sub`, `oauth_type`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_sub_oauth_type_key`(`sub`, `oauth_type`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
