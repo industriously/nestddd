02d6f2742ecbe376afae9621ebb6b5390695ac9e
industriously <dev.rojiwon@gmail.com>
2023-03-04 23:45:09 +0900

    DBClientFactory - ProviderBuilder 적용

--- src/infrastructure/DB/client.prisma.ts
+++ src/infrastructure/DB/client.prisma.ts
@@ -1,15 +1,18 @@
 import { DBClient } from '@INTERFACE/common';
+import { ProviderBuilder } from '@UTIL';
 import { getNamespace } from 'cls-hooked';
 import { PrismaService } from './prisma.service';
 import { TRANSACTION_CLIENT, TRANSACTION_NS_KEY } from './transaction';
 
-export const ClientFactory = (prisma: PrismaService): DBClient => ({
-  get() {
-    const namespace = getNamespace(TRANSACTION_NS_KEY);
-    if (!namespace?.active) {
-      throw Error('Namespace is not active');
-    }
+export const ClientFactory = (prisma: PrismaService): DBClient => {
+  return ProviderBuilder<DBClient>({
+    get() {
+      const namespace = getNamespace(TRANSACTION_NS_KEY);
+      if (!namespace?.active) {
+        throw Error('Namespace is not active');
+      }
 
-    return namespace.get(TRANSACTION_CLIENT) ?? prisma;
-  },
-});
+      return namespace.get(TRANSACTION_CLIENT) ?? prisma;
+    },
+  }).build();
+};
